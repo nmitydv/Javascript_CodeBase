f5d80726966150ca30eb331d807ec3f55e32054f
Namit Yadav <yadavnamit605@gmail.com>
2024-05-30 02:41:42 +0530

    some varialble naming convention which help's in career.

---
+++ storing_the_information_you_need/some_of_naming_convention.ts
@@ -0,0 +1,18 @@
+//DO
+var myAge;
+var init;
+var age;
+var ruleRegulation;
+var audio1;
+var audioOne;
+
+
+//Don't
+var 1;
+var a;
+var var;
+var MYAGE;
+var a;dsfljlasfj;
+var 1age;
+var 122;
+var adsfl;
