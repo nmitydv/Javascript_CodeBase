e1810a3e1ece22095fec9f5f7e8d8458afa521cd
Namit Yadav <yadavnamit605@gmail.com>
2024-05-30 02:20:23 +0530

    in const hositing is not working

--- storing_the_information_you_need/hosting.ts
+++ storing_the_information_you_need/hosting.ts
@@ -1,9 +1,14 @@
+//firstly we try on var because hosting is supported by var.
+
 var myName = "Namit";
 console.log(myName);
 var myName = "yadav";
 console.log(myName);
 
+// let we try on let variable
 
-// let myName1 = "Namit";
-// let myName1 = "yadav";
+let myName1 = "Namit";
+console.log(myName1);
+let myName1 = "yadav";
+console.log(myName1);
 
