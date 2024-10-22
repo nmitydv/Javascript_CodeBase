5390e0fc8ec8ef5d00754a9acf953d19d56d7bdd
Namit <yadavnamit605@gmail.com>
2024-10-23 02:56:46 +0530

    23

---
+++ Oct-23/first.js
@@ -0,0 +1,10 @@
+const num = 2;
+function square(num){
+    value = num*num;
+    return value;
+}
+
+var square2 = square(num);
+console.log(square2);
+var square4 = square(4);
+console.log(square4)
